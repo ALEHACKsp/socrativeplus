f30016c2a1cf83f76172d69fc53b07a31b883f4d
Samuel Tulach <samtulach@gmail.com>
2020-04-23 22:37:16 +0200

    gutten morgen

---
+++ client/External.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
+    public class External
+    {
+        private MainForm _form;
+        private ListBox _log;
+
+        public int answerCount;
+
+        public External(MainForm form, ListBox log)
+        {
+            _form = form;
+            _log = log;
+        }
+
+        public void WriteLog(string text)
+        {
+            if (_form.InvokeRequired)
+            {
+                Action<string> m = WriteLog;
+                _form.Invoke(m, text);
+                return;
+            }
+
+            string time = $"[{DateTime.Now.ToString("HH:mm:ss")}] ";
+            _log.Items.Add(time + text);
+        }
+
+        public string GetName()
+        {
+            TextInputForm input = new TextInputForm();
+            
+            DialogResult diag = input.ShowDialog();         
+            if (diag == DialogResult.OK)
+            {
+                return input.outputText;
+            } else
+            {
+                return "Nespecifikované";
+            }
+        }
+
+        public void SetAnswerCount(int number)
+        {
+            answerCount = number;
+        }
+    }
+}
---
+++ client/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class MainForm : Form
+    {
+        private External _ext;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InjectScript()
+        {
+            HtmlElement head = webBrowser1.Document.GetElementsByTagName("head")[0];
+            HtmlElement script = webBrowser1.Document.CreateElement("script");
+            script.SetAttribute("src", "https://tulach.cc/socrative/internal.js");
+            head.AppendChild(script);
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            InjectScript();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            webBrowser1.AllowWebBrowserDrop = false;
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.WebBrowserShortcutsEnabled = false;
+
+            _ext = new External(this, listBox1);
+            webBrowser1.ObjectForScripting = _ext;
+
+            timer1.Start();
+        }
+
+        private string[] GetAnswers()
+        {
+            try
+            {
+                string answerlist = webBrowser1.Document.InvokeScript("GetAnswersExternal").ToString();
+                string[] split = answerlist.Split(new string[] { "<ANSWER>" }, StringSplitOptions.None);
+                return split;
+            } catch (Exception error)
+            {
+                return new string[] { error.Message };
+            }
+        }
+
+        private void UpdateAnswers()
+        {
+            if (this.InvokeRequired)
+            {
+                Action m = UpdateAnswers;
+                this.Invoke(m);
+                return;
+            }
+
+            listBox2.Items.Clear();
+            listBox2.Items.Add("Celkem odpovědí: " + _ext.answerCount);
+
+            string[] answers = GetAnswers();
+            foreach (string answer in answers)
+            {
+                listBox2.Items.Add(answer);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateAnswers();
+        }
+    }
+}
